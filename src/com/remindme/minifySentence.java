b46ac7b29fda0a271dc20cd2c138a51720eefde5
Jake Eger <jakeeger@gmail.com>
2014-03-25 22:14:04 -0400

    I knew there was a better way.

--- src/com/remindme/minifySentence.java
+++ src/com/remindme/minifySentence.java
@@ -10,65 +10,66 @@ public class minifySentence {
 		
 		sentence = sentence.toLowerCase();
 		
-		sentence = sentence.replaceAll("\\bthe\\b", "");
-		sentence = sentence.replaceAll("\\bthis\\b", "");
-		sentence = sentence.replaceAll("\\bwas\\b", "");
-		sentence = sentence.replaceAll("\\bput\\b", "");
-		sentence = sentence.replaceAll("\\ba\\b", "");
-		sentence = sentence.replaceAll("\\ban\\b", "");
-		sentence = sentence.replaceAll("\\bas\\b", "");
-		sentence = sentence.replaceAll("\\band\\b", "");
-		sentence = sentence.replaceAll("\\bbe\\b", "");
-		sentence = sentence.replaceAll("\\bhave\\b", "");
-		sentence = sentence.replaceAll("\\bin\\b", "");
-		sentence = sentence.replaceAll("\\bto\\b", "");
-		sentence = sentence.replaceAll("\\bit\\b", "");
-		sentence = sentence.replaceAll("\\bto\\b", "");
-		sentence = sentence.replaceAll("\\bfor\\b", "");
-		sentence = sentence.replaceAll("\\bi\\b", "");
-		sentence = sentence.replaceAll("\\bthat\\b", "");
-		sentence = sentence.replaceAll("\\byou\\b", "");
-		sentence = sentence.replaceAll("\\bhe\\b", "");
-		sentence = sentence.replaceAll("\\bshe\\b", "");
-		sentence = sentence.replaceAll("\\bon\\b", "");
-		sentence = sentence.replaceAll("\\bwith\\b", "");
-		sentence = sentence.replaceAll("\\bdo\\b", "");
-		sentence = sentence.replaceAll("\\bat\\b", "");
-		sentence = sentence.replaceAll("\\bby\\b", "");
-		sentence = sentence.replaceAll("\\bwhere\\b", "");
-		sentence = sentence.replaceAll("\\bis\\b", "");
-		sentence = sentence.replaceAll("\\bthere\\b", "");
-		sentence = sentence.replaceAll("\\bhis\\b", "");
-		sentence = sentence.replaceAll("\\bher\\b", "");
-		sentence = sentence.replaceAll("\\bor\\b", "");
-		sentence = sentence.replaceAll("\\bwhich\\b", "");
-		sentence = sentence.replaceAll("\\bwe\\b", "");
-		sentence = sentence.replaceAll("\\bsay\\b", "");
-		sentence = sentence.replaceAll("\\bwill\\b", "");
-		sentence = sentence.replaceAll("\\bwould\\b", "");
-		sentence = sentence.replaceAll("\\bcan\\b", "");
-		sentence = sentence.replaceAll("\\bif\\b", "");
-		sentence = sentence.replaceAll("\\btheir\\b", "");
-		sentence = sentence.replaceAll("\\bgo\\b", "");
-		sentence = sentence.replaceAll("\\bwhat\\b", "");
-		sentence = sentence.replaceAll("\\bat\\b", "");
-		sentence = sentence.replaceAll("\\bsome\\b", "");
-		sentence = sentence.replaceAll("\\bcould\\b", "");
-		sentence = sentence.replaceAll("\\bdid\\b", "");
-		sentence = sentence.replaceAll("\\bleave\\b", "");
-		sentence = sentence.replaceAll("\\bleft\\b", "");
-		sentence = sentence.replaceAll("\\bmy\\b", "");
-		sentence = sentence.replaceAll("\\bthese\\b", "");
-		sentence = sentence.replaceAll("\\bright\\b", "");
-		sentence = sentence.replaceAll("\\blike\\b", "");
-		sentence = sentence.replaceAll("\\bgood\\b", "");
-		sentence = sentence.replaceAll("\\bgreat\\b", "");
-		sentence = sentence.replaceAll("\\bany\\b", "");
-		sentence = sentence.replaceAll("\\bnew\\b", "");
-		sentence = sentence.replaceAll("\\bvery\\b", "");
-		sentence = sentence.replaceAll("\\bhow\\b", "");
-		sentence = sentence.replaceAll("\\bwhen\\b", "");
-		sentence = sentence.replaceAll("\\bfind\\b", "");
+		sentence = sentence.replaceAll("\\bthe\\b" + 
+		"|\\bthis\\b" + 
+		"|\\bwas\\b" + 
+		"|\\bput\\b" + 
+		"|\\ba\\b" + 
+		"|\\ban\\b" + 
+		"|\\bas\\b" + 
+		"|\\band\\b" + 
+		"|\\bbe\\b" + 
+		"|\\bhave\\b" + 
+		"|\\bin\\b" + 
+		"|\\bto\\b" + 
+		"|\\bit\\b" + 
+		"|\\bto\\b" + 
+		"|\\bfor\\b" + 
+		"|\\bi\\b" + 
+		"|\\bthat\\b" + 
+		"|\\byou\\b" + 
+		"|\\bhe\\b" + 
+		"|\\bshe\\b" + 
+		"|\\bon\\b" + 
+		"|\\bwith\\b" + 
+		"|\\bdo\\b" + 
+		"|\\bat\\b" + 
+		"|\\bby\\b" + 
+		"|\\bwhere\\b" + 
+		"|\\bis\\b" + 
+		"|\\bthere\\b" + 
+		"|\\bhis\\b" + 
+		"|\\bher\\b" + 
+		"|\\bor\\b" + 
+		"|\\bwhich\\b" + 
+		"|\\bwe\\b" + 
+		"|\\bsay\\b" + 
+		"|\\bwill\\b" + 
+		"|\\bwould\\b" + 
+		"|\\bcan\\b" + 
+		"|\\bif\\b" + 
+		"|\\btheir\\b" + 
+		"|\\bgo\\b" + 
+		"|\\bwhat\\b" + 
+		"|\\bat\\b" + 
+		"|\\bsome\\b" + 
+		"|\\bcould\\b" + 
+		"|\\bdid\\b" + 
+		"|\\bleave\\b" + 
+		"|\\bleft\\b" + 
+		"|\\bmy\\b" + 
+		"|\\bthese\\b" + 
+		"|\\bright\\b" + 
+		"|\\blike\\b" + 
+		"|\\bgood\\b" + 
+		"|\\bgreat\\b" + 
+		"|\\bany\\b" + 
+		"|\\bnew\\b" + 
+		"|\\bvery\\b" + 
+		"|\\bhow\\b" + 
+		"|\\bwhen\\b" + 
+		"|\\bfind\\b"+ 
+		"", "");
 
 		
 		String newSentence = "";
